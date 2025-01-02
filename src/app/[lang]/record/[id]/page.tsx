dd315681e3cabdfc4bacf436be328df53103165d
shimizuSarun <sarun@sansenshimizu.com>
2025-01-02 17:22:16 +0900

    added i18-next(input text and column only)

--- src/app/[lang]/record/[id]/page.tsx
+++ src/app/[lang]/record/[id]/page.tsx
@@ -4,10 +4,12 @@ import { useRouter } from 'next/navigation'
 import { Box, Button, TextField, Typography } from '@mui/material'
 import TextFieldBox from '@/components/TextFieldBox'
 import useEditRecord from './hooks/useEditRecord'
+import { useTranslation } from 'react-i18next'
 
 export default function RecordDetail({ params }: any) {
   const router = useRouter()
   const { handleChange, recordData } = useEditRecord()
+  const { t } = useTranslation('record-id')
 
   return (
     <Box display={'flex'} flex={1} padding={1}>
@@ -38,112 +40,117 @@ export default function RecordDetail({ params }: any) {
         }}
       >
         <TextFieldBox
-          text='Drawing Number'
+          text={t('inputField1')}
           value={recordData?.drawingNumber}
           onChange={e => handleChange('drawingNumber', e.target.value)}
         />
         <TextFieldBox
-          text='Name'
+          text={t('inputField2')}
           value={recordData?.name}
           onChange={e => handleChange('name', e.target.value)}
         />
         <TextFieldBox
-          text='Material Cost'
+          text={t('inputField3')}
           value={recordData?.materialCost}
           onChange={e => handleChange('materialCost', e.target.value)}
         />
         <TextFieldBox
-          text='Material Supplier'
+          text={t('inputField4')}
           value={recordData?.materialSup}
           onChange={e => handleChange('materialSup', e.target.value)}
         />
         <TextFieldBox
-          text='Lathe Cost'
+          text={t('inputField5')}
           value={recordData?.latheCost}
           onChange={e => handleChange('latheCost', e.target.value)}
         />
         <TextFieldBox
-          text='Lathe Supplier'
+          text={t('inputField6')}
           value={recordData?.latheSup}
           onChange={e => handleChange('latheSup', e.target.value)}
         />
         <TextFieldBox
-          text='Milling Cost'
+          text={t('inputField7')}
           value={recordData?.millingCost}
           onChange={e => handleChange('millingCost', e.target.value)}
         />
         <TextFieldBox
-          text='Milling Supplier'
+          text={t('inputField8')}
           value={recordData?.millingSup}
           onChange={e => handleChange('millingSup', e.target.value)}
         />
         <TextFieldBox
-          text='Heat Treatment Cost'
+          text={t('inputField9')}
           value={recordData?.heatTreatmentCost}
           onChange={e => handleChange('heatTreatmentCost', e.target.value)}
         />
         <TextFieldBox
-          text='Heat Treatment Supplier'
+          text={t('inputField10')}
           value={recordData?.heatTreatmentSup}
           onChange={e => handleChange('heatTreatmentSup', e.target.value)}
         />
         <TextFieldBox
-          text='Grinding Cost'
+          text={t('inputField11')}
           value={recordData?.grindingCost}
           onChange={e => handleChange('grindingCost', e.target.value)}
         />
         <TextFieldBox
-          text='Grinding Supplier'
+          text={t('inputField12')}
           value={recordData?.grindingSup}
           onChange={e => handleChange('grindingSup', e.target.value)}
         />
         <TextFieldBox
-          text='Transportation Cost'
+          text={t('inputField13')}
           value={recordData?.transportationCost}
           onChange={e => handleChange('transportationCost', e.target.value)}
         />
         <TextFieldBox
-          text='Transportation Supplier'
+          text={t('inputField14')}
           value={recordData?.transportationSup}
           onChange={e => handleChange('transportationSup', e.target.value)}
         />
         <TextFieldBox
-          text='General Cost'
+          text={t('inputField15')}
           value={recordData?.generalCost}
           onChange={e => handleChange('generalCost', e.target.value)}
         />
         <TextFieldBox
-          text='General Supplier'
+          text={t('inputField16')}
           value={recordData?.generalSup}
           onChange={e => handleChange('generalSup', e.target.value)}
         />
         <TextFieldBox
-          text='Welding Cost'
+          text={t('inputField17')}
           value={recordData?.weldingCost}
           onChange={e => handleChange('weldingCost', e.target.value)}
         />
         <TextFieldBox
-          text='Welding Supplier'
+          text={t('inputField18')}
           value={recordData?.weldingSup}
           onChange={e => handleChange('weldingSup', e.target.value)}
         />
         <TextFieldBox
-          text='Other Cost'
+          text={t('inputField19')}
           value={recordData?.otherCost}
           onChange={e => handleChange('otherCost', e.target.value)}
         />
         <TextFieldBox
-          text='Other Supplier'
+          text={t('inputField20')}
           value={recordData?.otherSup}
           onChange={e => handleChange('otherSup', e.target.value)}
         />
         <TextFieldBox
-          text='Defect Details'
+          text={t('inputField21')}
+          value={recordData?.sellingPrice}
+          onChange={e => handleChange('sellingPrice', e.target.value)}
+        />
+        <TextFieldBox
+          text={t('inputField22')}
           value={recordData?.defectDetails}
           onChange={e => handleChange('defectDetails', e.target.value)}
         />
         <Button variant='contained' onClick={() => console.log(recordData)}>
-          Save
+          {t('saveButton')}
         </Button>
       </Box>
     </Box>
