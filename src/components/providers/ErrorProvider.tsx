17101a26e8d73c285f482b0a9e1f4b46ee564877
Htet Htet <htethtet@sansenshimizu.com>
2025-03-05 15:13:47 +0900

    add contact info label sample and err msg

--- src/components/providers/ErrorProvider.tsx
+++ src/components/providers/ErrorProvider.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Box, Button, Container, Stack, Typography } from '@mui/material'
+import { Box, Button, Container, Divider, Link, Stack, Typography } from '@mui/material'
 import { usePathname, useRouter } from 'next/navigation'
 import { ErrorBoundary, FallbackProps } from 'react-error-boundary'
 import { useTranslation } from 'react-i18next'
@@ -18,17 +18,27 @@ const ErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => {
     <Box sx={{ mt: 8 }}>
       <Container>
         <Stack spacing={1} direction='row' sx={{ alignItems: 'center', mb: 2 }}>
-          <Typography variant='h6' color='error'>
+          <Typography variant='h5' color='error'>
             {t('error.title')}
           </Typography>
           <SentimentVeryDissatisfiedIcon color='error' />
         </Stack>
-        <Typography gutterBottom>
+        <Typography variant='h6' gutterBottom>
           {t('error.message', { pageName: t(`sideMenu.${getCurrentURLPath()}`) })}
+        </Typography>
+        <Typography variant='subtitle2' gutterBottom>
+          {t('error.errorMessageTitle')} : ({error.message})
         </Typography>
         <Button variant='contained' onClick={resetErrorBoundary} sx={{ my: 2 }}>
           {t('error.submitButton')}
         </Button>
+        <Divider />
+        <Stack spacing={1} direction='row' sx={{ alignItems: 'center', mt: 2 }}>
+          <Typography>{t('error.contactInfoTitle')}</Typography>
+          <Link href='#' variant='subtitle1' color='primary'>
+            {t('error.contactInfoMessage')}
+          </Link>
+        </Stack>
       </Container>
     </Box>
   )
