42be3120692b70b52d80cc4a21bbefed732add7d
shimizuSarun <sarun@sansenshimizu.com>
2025-04-10 16:04:38 +0900

    [Hot-Fix] dialog not auto close on save as success

--- src/app/[lang]/erase/page.tsx
+++ src/app/[lang]/erase/page.tsx
@@ -206,6 +206,7 @@ export default function ErasePage() {
       })
       const result = await addNewDrawing(file)
       if (result) {
+        setOpenModalAddNewDrawing(false)
         notificationSnackbar.success(t('addNewDrawingMessageSuccess'))
       }
     },
