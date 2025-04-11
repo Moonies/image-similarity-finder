4636c9b1fa9fed72c5c30263575abfdcd45dbdb2
shimizuSarun <sarun@sansenshimizu.com>
2025-04-11 09:23:07 +0900

    [Hot-Fix] redirect with localStorage and add condition support in i18next

--- src/app/page.ts
+++ src/app/page.ts
@@ -1,5 +1,23 @@
-import { redirect } from 'next/navigation'
+'use client'
+import { redirect, usePathname } from 'next/navigation'
+import { useEffect } from 'react'
+import i18next from '@/config/i18n'
+import { getCurrentLanguage } from '@/store/slices/httpSlice'
 
 export default function DefaultPage() {
-  redirect('/en')
+  const pathname = usePathname()
+  const [lang, _currentPath] = pathname.replace(/^\//, '').split('/')
+  const supportedLanguage = i18next.options.supportedLngs as string[]
+
+  useEffect(() => {
+    const currentLanguage = getCurrentLanguage()
+    if (currentLanguage) {
+      redirect(`/${currentLanguage}`)
+    } else if (supportedLanguage?.includes(lang)) {
+      redirect(`/${lang}`)
+    } else {
+      redirect('/en')
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [lang])
 }
